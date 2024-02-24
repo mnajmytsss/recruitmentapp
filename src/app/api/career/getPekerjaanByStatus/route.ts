450eeb2f5d818c3761bc62106ed245593fdd50d7
mnajmytsss <najmytsaqiby22@gmail.com>
2024-02-24 10:50:44 +0700

    feat: create api get pekerjaanByStatus

---
+++ src/app/api/career/getPekerjaanByStatus/route.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { connectToDB } from "@/dbConfig/dbConfig";
+import Pekerjaan from "@/models/pekerjaanModels";
+import { NextRequest, NextResponse } from "next/server";
+
+connectToDB();
+
+export async function GET(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const status = searchParams.get("status");
+
+    if (!status) {
+      return NextResponse.json(
+        { message: "Job status is required" },
+        { status: 400 }
+      );
+    }
+
+    const pekerjaan = await Pekerjaan.find({ status });
+
+    if (!pekerjaan) {
+      return NextResponse.json({ message: "Job not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(pekerjaan);
+  } catch (error) {
+    console.error("Error fetching job posting:", error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
