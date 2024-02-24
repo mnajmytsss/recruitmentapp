816822ab594beb5d8f353374400d921dcb10069d
mnajmytsss <najmytsaqiby22@gmail.com>
2024-02-24 10:52:08 +0700

    feat: create api update pekerjaan

---
+++ src/app/api/career/updatePekerjaan/route.ts
@@ -0,0 +1,56 @@
+"use server";
+
+import { connectToDB } from "@/dbConfig/dbConfig";
+import Pekerjaan from "@/models/pekerjaanModels";
+import { NextRequest, NextResponse } from "next/server";
+
+connectToDB();
+
+export async function PUT(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get("id");
+    const {
+      status,
+      pekerjaan,
+      deskripsi,
+      kualifikasi,
+      tanggungJawab,
+      manfaat,
+    } = await req.json();
+
+    if (!id || status === undefined) {
+      return NextResponse.json(
+        { message: "Job ID and status are required" },
+        { status: 400 }
+      );
+    }
+
+    const updateResult = await Pekerjaan.updateOne(
+      { _id: id },
+      {
+        $set: {
+          pekerjaan: pekerjaan,
+          deskripsi: deskripsi,
+          kualifikasi: kualifikasi,
+          tanggungJawab: tanggungJawab,
+          manfaat: manfaat,
+          status: status,
+          updatedAt: new Date(),
+        },
+      }
+    );
+
+    if (updateResult.matchedCount === 0) {
+      return NextResponse.json({ message: "Job not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ message: "Job status updated successfully" });
+  } catch (error) {
+    console.error("Error updating job status:", error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
